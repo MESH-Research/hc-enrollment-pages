87ea9d765e6405146d1e21616bd854a0746d25aa
= <lfulgencio@mla.org>
2016-09-23 17:12:34 -0400

    added global css for enrollment flows as well as header/footer partials for each view

---
+++ partials/footer.php
@@ -0,0 +1,7 @@
+<footer id="customFooter">
+  <div class="contentWidth">
+    <div class="contentFooterWithoutSidebar">
+      <p>test</p>
+    </div> <!-- /.contentFooterWithoutSidebar -->
+  </div> <!-- /.contentWidth -->
+</footer> <!-- /#customFooter -->
---
+++ partials/header.php
@@ -0,0 +1,13 @@
+<header id="customHeader">
+  <div class="contentWidth">
+    <!-- example markup for your header field -->
+    <h1>HC Custom Site Title</h1>
+    <div class="customElement">HC site element</div>
+    <div class="customImage"><img src="https://hcommons-dev.org/app/uploads/2016/09/hc_mla_65px.png" /></div>
+</header> <!-- /#customHeader -->
+
+<nav id="row1" aria-label="user and platform menus">
+  <div class="contentWidth">
+    <div id="secondaryMenu" class="rightmenu"></div> <!-- /.secondaryMenu -->
+  </div> <!-- /.contentWidth -->
+</nav> <!-- /#row1 -->
