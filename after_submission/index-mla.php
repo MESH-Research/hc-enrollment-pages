6635c62adbb276747f48c01de9fbd63f521bbce1
= <lfulgencio@mla.org>
2016-09-23 17:10:32 -0400

    added header/footers for after_submission enrollment flow

--- after_submission/index-mla.php
+++ after_submission/index-mla.php
@@ -6,19 +6,23 @@
   <link rel="stylesheet" type="text/css" href="/css/skeleton.css" />
   <link rel="stylesheet" type="text/css" href="/css/discovery_service_01.css" />
   <link rel="stylesheet" type="text/css" href="idpselect.css" />
+  <link rel="stylesheet" type="text/css" href="/css/global.css" />
 </head>
 
 <body>
+
+  <?php include('../partials/header.php'); ?>
+
   <div class="section">
     <div class="container">
       <div class="row">
         <h2>Please verify your e-mail address.</h2>
-	<p>Check your e-mail for a note with the subject "Activate your MLA Commons Account". In that message you will find a link. Please click on it so that we can verify your e-mail address and you can complete the registration process.</p>
-      </div>
-    </div>
-  </div>
+	       <p>Check your e-mail for a note with the subject "Activate your Humanities Commons Account". In that message you will find a link. Please click on it so that we can verify your e-mail address and you can complete the registration process.</p>
+      </div> <!-- /.row -->
+    </div> <!-- /.container -->
+  </div> <!-- /.section -->
 
-     
+  <?php include('../partials/footer.php'); ?>
 
 </body>
 </html>
