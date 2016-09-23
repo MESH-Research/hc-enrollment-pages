2ed549c1cfdf7994e4fc4803efb13f7c59804637
= <lfulgencio@mla.org>
2016-09-23 15:26:33 -0400

    updated gitignore for gulp, added gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp')
+    browserSync = require('browser-sync').create()
+    plumber = require('gulp-plumber'),
+    env = require('dotenv').config();
+
+gulp.task('serve', function() {
+
+  browserSync.init({
+    proxy: 'http://' + process.env.SITE_URL,
+    logLevel: "info",
+    files: [
+      'after_submission/',
+      'after_confirmation/',
+      'css/',
+      'discovery_service_enrollment/',
+      'discovery_service_registry/',
+      'img/',
+      {
+        fn: function( event, file ) {
+          this.reload();
+        }
+      }
+    ]
+  });
+
+});
