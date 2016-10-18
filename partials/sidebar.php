092f8a3635036c0cfecc47b9f7d0185d47bf70d3
Leo Fulgencio <leo.fulgencio@gmail.com>
2016-10-18 10:57:33 -0400

    more sidebar changes

--- partials/sidebar.php
+++ partials/sidebar.php
@@ -1,3 +1,9 @@
+<?php if( $registryUrl['dirname'] == '/discovery_service_wordpress' || $registryUrl['dirname'] == '/discovery_service_registry' || $registryUrl['dirname'] == '/discovery_service_registry_only' ) : ?>
 <div class="sidebar three columns align-self-center login_registry_text column-sidebar">
   <img src="/img/hub_graphic_240.png" class="center">
 </div>
+<?php else : ?>
+<div class="sidebar three columns align-self-center column-sidebar">
+  <img src="/img/hub_graphic_240.png" class="center">
+</div>
+<?php endif; ?>
