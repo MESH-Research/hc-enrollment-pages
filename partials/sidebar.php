8d85a4515d57f6c73bc1b47b6a0335f154c4c575
Leo Fulgencio <leo.fulgencio@gmail.com>
2016-11-10 14:29:46 -0500

    added new svg image css changes for sidebar, removed conditional logic

--- partials/sidebar.php
+++ partials/sidebar.php
@@ -1,9 +1,3 @@
-<?php if( $registryUrl['dirname'] == '/discovery_service_wordpress' || $registryUrl['dirname'] == '/discovery_service_registry' || $registryUrl['dirname'] == '/discovery_service_registry_only' ) : ?>
-<div class="sidebar three columns align-self-center login_registry_text column-sidebar">
-  <img src="/img/hub_graphic_240.png" class="center">
-</div>
-<?php else : ?>
 <div class="sidebar three columns align-self-center column-sidebar">
-  <img src="/img/hub_graphic_nov3.svg" class="center">
+  <img src="/img/hub_graphic_large_nov10_1.svg" class="center">
 </div>
-<?php endif; ?>
