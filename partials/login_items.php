d9244ef025e3aee43fc57c6e673253be35be138d
Leo Fulgencio <leo.fulgencio@gmail.com>
2016-12-08 10:25:44 -0500

    updated if check for cakephp on login_items for new hc account link

--- partials/login_items.php
+++ partials/login_items.php
@@ -97,7 +97,7 @@ function outputUrl( $entityId, $newLogin = false ) {
 
   <?php
     //we only want the user to see this option when in the discovery_service_registry enrollment flow
-    if( $check_saml_cake['CAKEPHP'] == false && $check_saml_cake['_saml_idp'] == true &&
+    if( $check_saml_cake['CAKEPHP'] == true && $check_saml_cake['_saml_idp'] == false &&
     $registryUrl['dirname'] == '/discovery_service_registry' ||
     $check_saml_cake['CAKEPHP'] == true && $check_saml_cake['_saml_idp'] == true &&
     $registryUrl['dirname'] == '/discovery_service_registry') :
