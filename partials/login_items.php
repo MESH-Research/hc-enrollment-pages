c9e8fb05fc030b07ac1907588a9331441af64d43
Leo Fulgencio <leo.fulgencio@gmail.com>
2016-12-08 14:10:50 -0500

    changed if statement to display row on cakephp cookie to true

--- partials/login_items.php
+++ partials/login_items.php
@@ -61,7 +61,7 @@ function outputUrl( $entityId, $newLogin = false ) {
     */ ?>
   </div> <!-- /.row.with-flex -->
 
-  <?php if( $check_saml_cake['CAKEPHP'] == false && $check_saml_cake['_saml_idp'] == true &&
+  <?php if( $check_saml_cake['CAKEPHP'] == true && $check_saml_cake['_saml_idp'] == false &&
   $registryUrl['dirname'] == '/discovery_service_registry' ||
   $check_saml_cake['CAKEPHP'] == true && $check_saml_cake['_saml_idp'] == true &&
   $registryUrl['dirname'] == '/discovery_service_registry' ) : ?>
