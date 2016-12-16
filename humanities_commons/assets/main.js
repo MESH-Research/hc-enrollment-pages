7f84750f896b188ba8ac82bee13df91aa39f1891
Leo Fulgencio <leo.fulgencio@gmail.com>
2016-12-16 15:04:06 -0500

    added humanities_commons folder that contains code for comanage themes

---
+++ humanities_commons/assets/main.js
@@ -0,0 +1,12 @@
+<script type="text/javascript">
+$(document).ready(function() {
+$('.sidebar').prepend(
+     $('<img/>', { class: 'center', src: '/img/hub_graphic_large_nov10_1.svg' })
+);
+});
+</script>
+
+<script type='text/javascript' src='//use.typekit.net/bgx6tpq.js'></script>
+<script type='text/javascript'>
+     try{Typekit.load();}catch(e){};
+</script>
