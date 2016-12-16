ef14a689761549da4ba5afddd69ddd0cca8157ca
Ryan Williams <ryrowi@gmail.com>
2016-12-16 14:56:52 -0500

    include idpselect_languages.js

--- partials/incommon.php
+++ partials/incommon.php
@@ -10,6 +10,7 @@
 				'<?php echo HC_ACCOUNT_CREATE_GATEWAY; ?>',
 			];
 		</script>
+		<script src="/lib/shibboleth-embedded-ds-1.1.0/idpselect_languages.js" type="text/javascript" language="javascript"></script>
 		<script src="/lib/shibboleth-embedded-ds-1.1.0/idpselect_config.js" type="text/javascript" language="javascript"></script>
 		<script src="/lib/shibboleth-embedded-ds-1.1.0/idpselect.js" type="text/javascript" language="javascript"></script>
 		<noscript>
