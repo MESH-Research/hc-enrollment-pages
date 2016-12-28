d40ee37c2545692f144083a4e455949d127694f4
Leo Fulgencio <leo.fulgencio@gmail.com>
2016-12-28 11:42:13 -0500

    added global css for enrollment specific styles, updated main.js file

--- humanities_commons/assets/main.js
+++ humanities_commons/assets/main.js
@@ -3,6 +3,11 @@ $(document).ready(function() {
 $('.sidebar').prepend(
      $('<img/>', { class: 'center', src: '/img/hub_graphic_large_nov10_1.svg' })
 );
+
+$('head').append('<meta name="viewport" content="width=device-width" />');
+$('body').addClass( society + '-enrollment');
+$('head').append( $('<link />', { rel: "stylesheet", type: "text/css", href: "/humanities_commons/assets/global.css" }) );
+
 });
 </script>
 
