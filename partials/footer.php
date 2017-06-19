d04066b987e1bde6ea2f7aa1ed258bfa8102e3a4
Leo Fulgencio <leo.fulgencio@gmail.com>
2017-06-19 14:27:54 -0400

    updated footer year and removed bottom margin on .container

--- partials/footer.php
+++ partials/footer.php
@@ -1,7 +1,7 @@
 <footer id="customFooter">
   <div class="contentWidth">
     <div class="contentFooterWithoutSidebar">
-        <p>Copyright &copy; 2016 MLA. Powered By COmanage.</p>
+        <p>Copyright &copy; <?php echo date('Y'); ?> MLA. Powered By COmanage.</p>
     </div> <!-- /.contentFooterWithoutSidebar -->
   </div> <!-- /.contentWidth -->
 </footer> <!-- /#customFooter -->
